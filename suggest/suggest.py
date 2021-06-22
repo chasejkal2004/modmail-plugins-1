7d0edccb86b58c44a53cadc73dc0a91568c60583
chasejkal2004 <54160880+chasejkal2004@users.noreply.github.com>
2021-06-21 22:23:15 -0500

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -102,7 +102,7 @@ class Suggest(commands.Cog):
             
     @suggest.error
     async def suggest_error(ctx, error):
-        if isinstance(error, commands.MissingRole):
+        if isinstance(error, commands.errors.MissingRole):
             await ctx.send("You cant do that!")
     @commands.command()
     @checks.has_permissions(PermissionLevel.ADMIN)
