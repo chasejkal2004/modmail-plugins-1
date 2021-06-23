860bd975a19615df9582800c58b5582d3798f7ee
chasejkal2004 <54160880+chasejkal2004@users.noreply.github.com>
2021-06-23 17:59:02 -0500

    Update say.py

--- say/say.py
+++ say/say.py
@@ -4,12 +4,6 @@ from discord.ext import commands
 class Say(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.command()
-    async def say2(self, ctx, *, message):
-        """ModMail says what you want it to say."""
-        await ctx.send(message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
-        await ctx.message.delete()
 
 
     @commands.command(pass_context=True)
