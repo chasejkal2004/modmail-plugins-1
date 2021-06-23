025ee9a68b09e6a385fc0ec6c717eb40b3ddac39
chasejkal2004 <54160880+chasejkal2004@users.noreply.github.com>
2021-06-23 18:03:55 -0500

    Update say.py

--- say/say.py
+++ say/say.py
@@ -9,8 +9,8 @@ class Say(commands.Cog):
     @commands.command(pass_context=True)
     async def addrole(ctx):
         thread = ctx.thread
-        member = thread.recipient
-        role = get(member.server.roles, name="Member")
-        await ctx.add_roles(member, role)
+        user = thread.recipient
+        role = discord.utils.get(user.guild.roles, name="Trusted")
+        await user.add_roles(role)
 def setup(bot):
     bot.add_cog(Say(bot))
